e1a5118a56d041be11fbc0e3bef53e9c2a5f52c1
Tomaszsachanowski <tomaszsachanowski@gmail.com>
2021-11-12 00:48:02 +0100

    Add installation file #1

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+
+    name="Master_thesis",
+    version="0.1",
+    description="Applications of machine learning algorithms to remove noise and artifacts present in brain MRI records",
+    author=['Tomasz Sachanowski'],
+    packages=setuptools.find_packages(),
+    install_requires=['numpy','dataframe', 'nibabel']
+)
