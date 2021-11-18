a8ad4ec86ce3fa52437ee3ed83acf9b7ba434956
Tomaszsachanowski <tomaszsachanowski@gmail.com>
2021-11-18 13:33:21 +0100

    Test add line into image

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 mri_images_data = MriImage.load()
-MriImage.save(mri_images_data)
+data = mri_images_data[100]
+plt.imshow(data, cmap="gray", origin="lower")
+plt.plot((0,255),(127, 127),color="r", linewidth=2)
+plt.show()
+# MriImage.save(mri_images_data)
