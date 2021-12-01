9b9ad7f870bfac40137c6ec955e68e955df99c33
Tomaszsachanowski <tomaszsachanowski@gmail.com>
2021-12-01 23:01:01 +0100

    save day commit

--- master_thesis/generator/ArtifactsGenerator.py
+++ master_thesis/generator/ArtifactsGenerator.py
@@ -1,7 +1,7 @@
 
 from random import Random
-from math import radians, sin, cos, ceil, floor
-
+from math import degrees, radians, sin, cos, ceil, floor
+from numpy import round
 from config import Config
 from PIL import Image, ImageDraw, ImageFilter
 
@@ -25,6 +25,7 @@ class ArtifactsGenerator:
     def blur(self, pos_x_0, pos_y_0, pos_x_1, pos_y_1):
         region_blured = self.image.crop((pos_x_0, pos_y_0, pos_x_1, pos_y_1))
         region_blured = region_blured.filter(ImageFilter.GaussianBlur(1))
+        # region_blured = region_blured.filter(ImageFilter.MedianFilter(3))
         self.image.paste(region_blured, (pos_x_0, pos_y_0, pos_x_1, pos_y_1))
 
     def draw_gradient_elipse(self, pos_x, pos_y, radious_range, gradient_range, transparent_range, levels):
@@ -48,12 +49,12 @@ class ArtifactsGenerator:
             image_draw_elipse.ellipse((elipse_x_0, elipse_y_0, elipse_x_1, elipse_y_1),
                                     fill=color, outline=color)
 
-        # Rectangle for blur
-        elipse_x_0 = floor(pos_x - radious_range[1]) 
-        elipse_x_1 = ceil(pos_x + radious_range[1])
-        elipse_y_0 =  floor(pos_y - radious_range[1])
-        elipse_y_1 =  ceil(pos_y + radious_range[1])
-        self.blur(elipse_x_0, elipse_y_0, elipse_x_1, elipse_y_1)
+        # # Rectangle for blur
+        # elipse_x_0 = floor(pos_x - radious_range[1]) 
+        # elipse_x_1 = ceil(pos_x + radious_range[1])
+        # elipse_y_0 =  floor(pos_y - radious_range[1])
+        # elipse_y_1 =  ceil(pos_y + radious_range[1])
+        # self.blur(elipse_x_0, elipse_y_0, elipse_x_1, elipse_y_1)
 
     def draw_gradient_triangle(self, pos_x, pos_y, high_range, alpha, beta, gradient_range, transparent_range, levels):
         gradient_min, gradient_max = gradient_range
@@ -142,9 +143,12 @@ class ArtifactsGenerator:
         gradient_range = ARTIFACTS_ELIPSE["GRADIENT_RANGE"]
         transparent_range = ARTIFACTS_ELIPSE["TRANSPARENT_RANGE"]
         levels = ARTIFACTS_ELIPSE["LEVELS"]
+        degree = randomizer.uniform(305, 295)
+        self.generate_prazki(pos_x_1, pos_y_1, degree)
         self.draw_gradient_elipse(pos_x_1, pos_y_1, radious_range, gradient_range,
                                   transparent_range, levels)
         self.generate_star_arms(pos_x_1, pos_y_1)
+
 
         # SECOND ELIPSE
         pos_x_2 = pos_x_1 + randomizer.uniform(-2.0, 2.0)
@@ -154,8 +158,61 @@ class ArtifactsGenerator:
         gradient_range = ARTIFACTS_ELIPSE["GRADIENT_RANGE"]
         transparent_range = ARTIFACTS_ELIPSE["TRANSPARENT_RANGE"]
         levels = ARTIFACTS_ELIPSE["LEVELS"]
+        degree = randomizer.uniform(265, 255)
+        self.generate_prazki(pos_x_2, pos_y_2, degree)
         self.draw_gradient_elipse(pos_x_2, pos_y_2, radious_range, gradient_range,
                                   transparent_range, levels)
         self.generate_star_arms(pos_x_2, pos_y_2)
+        # for i in range(255):
+        #     print(pixels[127,i])
 
+        #     pixels[127,i] = tuple([pixels[127,i][0]+20, pixels[127,i][1]+20, pixels[127,i][2]+20, 255])
+            # print(pixels[127,i])
         self.image.show()
+        # return self.image
+
+    def generate_prazki(self, pos_x, pos_y, degree):
+        randomizer = self.randomizer()
+        max_high = randomizer.uniform(100, 90)
+        min_high = randomizer.uniform(20, 10)
+        alpha = radians(degree)
+        point_1 = pos_x + max_high * sin(alpha), pos_y - max_high * cos(alpha)
+    
+        all_xy = []
+        for j in range(8):
+            for i in range(5):
+                alpha = radians(degree-0.3*i-0.3*5*j)
+                point_1 = pos_x + max_high * sin(alpha), pos_y - max_high * cos(alpha)
+
+                d = pos_x - point_1[0]
+                b = pos_y - (point_1[1]+i)
+                dy = b/d
+                xn, yn =  int(round(point_1[0])), int(round(point_1[1]))
+                tmp = yn
+                for i in range(int(round(d))):
+                    if (xn, yn) not in all_xy:
+                        if (j%2) == 0:
+                            self.increase_pixel_value(xn, yn)
+                        else:
+                                self.decrease_pixel_value(xn, yn)
+                    xn = xn + 1
+                    tmp = tmp + dy
+                    yn = int(round(tmp))
+                    if xn > pos_x - min_high:
+                        break
+
+    def increase_pixel_value(self, x, y):
+        pixels = self.image.load()
+        R, G, B, T =  pixels[x, y]
+        R = R + 10
+        if R > 255:
+            R = 255
+        pixels[x,y] = tuple([R, R, R, 255])
+
+    def decrease_pixel_value(self, x, y):
+        pixels = self.image.load()
+        R, G, B, T =  pixels[x, y]
+        R = R - 10
+        if R == 0:
+            R = 0
+        pixels[x,y] = tuple([R, R, R, 255])
